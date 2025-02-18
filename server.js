9ecf13995bec81200e48155c337b1aff066efe4d
Pomboon Pomboonmee <79350873+Pmoarctic@users.noreply.github.com>
2025-02-18 16:14:31 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,2 +1,2 @@
 PORT = 3000;
-MONGO_URI = "mongodb+srv://Pombun:QT3beDLTmp1hHjWj@vacqcluster.aewzw.mongodb.net/?retryWrites=true&w=majority&appName=VacQCluster";
+MONGO_URI = "mongodb+srv://Pombun:<password>@vacqcluster.aewzw.mongodb.net/?retryWrites=true&w=majority&appName=VacQCluster";
